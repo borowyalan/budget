fb2caaaa4a463673bf51996bf8de3de88ff9a5e1
Alan Borowy <borowyalan@gmail.com>
2019-10-29 16:49:01 +0100

    remove unnecessary return option

--- src/components/Auth/Authentication.js
+++ src/components/Auth/Authentication.js
@@ -4,7 +4,7 @@ import CurrentUser from "./CurrentUser";
 import SignIn from "./SignIn";
 
 const Authentication = ({ user, loading }) => {
-	if (loading) return null;
+	
 	return <>{user ? <CurrentUser {...user} /> : <SignIn />}</>;
 };
 
