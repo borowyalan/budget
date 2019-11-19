abdb25cb14c51cc04f8fc16cdbd89a509fa77dee
Alan Borowy <borowyalan@gmail.com>
2019-11-19 11:27:44 +0100

    don't show login screen when waiting for user

--- src/App.js
+++ src/App.js
@@ -8,9 +8,17 @@ import { UserContext } from "./providers/UserProvider";
 function App() {
 	const user = useContext(UserContext);
 
+	console.log(user);
+
 	return (
 		<div className='App'>
-			{user ? <Expenses user={user} /> : <Authentication />}
+			{user ? (
+				<Expenses user={user} />
+			) : user === undefined ? (
+				<Authentication />
+			) : (
+				""
+			)}
 		</div>
 	);
 }
